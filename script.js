fbcd0d1717a0bcb71567f9058b3caca9f40cb19f
rubens-rodrigues <misterubens@gmail.com>
2024-01-12 13:53:34 -0300

    Final

---
+++ script.js
@@ -0,0 +1,21 @@
+
+// script pra o Acordeon da FAQ
+document.addEventListener("DOMContentLoaded", function () {
+  const faqQuestions = document.querySelectorAll(".faq-question");
+
+  faqQuestions.forEach(function (question) {
+      question.addEventListener("click", function () {
+          const answer = this.nextElementSibling;
+
+          if (answer.style.display === "block") {
+              answer.style.display = "none";
+              this.classList.remove("open");
+          } else {
+              answer.style.display = "block";
+              this.classList.add("open");
+          }
+      });
+  });
+});
+
+
