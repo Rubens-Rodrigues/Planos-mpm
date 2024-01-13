465c8aa3d5b7f209f7b1ae3ac3e7bfb1a79e5c42
Alvaro Bacelar <Alvaro.Sousa@syngenta.com>
2024-01-13 16:29:14 -0300

    Adicionado banco de dados

--- consultproject/settings.py
+++ consultproject/settings.py
@@ -75,13 +75,21 @@ WSGI_APPLICATION = "consultproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "my.cnf",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
