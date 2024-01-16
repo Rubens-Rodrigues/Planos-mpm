552889c8ca81893e2419c80923dcdb1a24825b58
Alvaro Bacelar <Alvaro.Sousa@syngenta.com>
2024-01-16 10:42:40 -0300

    Adicionado o formulario de envio de dados para o backend

---
+++ landingpage/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.forms import ModelForm
+from . models import LeadsForm, TITLE_CHOICES
+
+class LeadsFomul(ModelForm):
+    class Meta:
+        model = LeadsForm
+        fields = ('nome', 'email', 'phone', 'leadsource')
+        labels = {
+            'nome': '',
+            'email': '',
+            'phone': '',
+            'leadsource': '',
+        }
+        widgets = {
+            'nome': forms.TextInput(attrs={'placeholder': 'Seu nome', 'class':'lead-form'}),
+            'email': forms.TextInput(attrs={'placeholder': 'Seu melhor e-mail'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Seu Whatsapp'}),
+            'leadsource': forms.TextInput(attrs={'type': 'hidden', 'value': 'Landing Page'}),
+        }
+    
--- landingpage/views.py
+++ landingpage/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import get_object_or_404,render
 from store.models import Product, Category
+from django.http import HttpResponseRedirect
+from . forms import LeadsFomul
+
 
 # Create your views here.
 
 def index(request):
+    submitted = False
+    if request.method == "POST":
+        lead = LeadsFomul(request.POST)
+        if lead.is_valid():
+            lead.save()
+            return HttpResponseRedirect('/?submitted=True#form')
+    else:
+        lead = LeadsFomul
+        if 'submitted' in request.GET:
+            submitted = True
     
     products=Product.objects.filter(status=True).values()
     categories=Category.objects.all().order_by('name')
-    # if categories != None:
-    #     category_page=get_object_or_404(store.Category)
-    #     products=store.Product.objects.filter(category=category_page, status=True)
-
+    
     context={
         'products':products,
         'categories':categories,
-        'title': 'Positive-se Mulher'
+        'title': 'Positive-se Mulher',
+        'form': LeadsFomul,
+        'submitted': submitted
     }
     
     return render(request, 'home.html', context)
+
+# def index(request):
+    
+#     lead = LeadsForm(request.POST)
+#     new_lead = lead.save()
