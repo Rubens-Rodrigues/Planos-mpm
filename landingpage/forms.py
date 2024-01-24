16c086b7a52d52e93c44c94519f29b27fd33ebcc
Alvaro Bacelar <Alvaro.Sousa@syngenta.com>
2024-01-24 12:37:55 -0300

    Adicionado o formulario para captura de pre-inscricao

--- landingpage/forms.py
+++ landingpage/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from . models import LeadsForm, TITLE_CHOICES
+from . models import LeadsForm, TITLE_CHOICES, PreInscricao
 
 class LeadsFomul(ModelForm):
     class Meta:
@@ -17,4 +17,17 @@ class LeadsFomul(ModelForm):
             'email': forms.EmailInput(attrs={'placeholder': 'Seu melhor e-mail'}),
             'phone': forms.TextInput(attrs={'placeholder': 'Seu Whatsapp - (99) 9999-9999', 'title': 'Informe um número de telefone válido'}),
             'leadsource': forms.TextInput(attrs={'type': 'hidden', 'value': 'Landing Page'}),
+        }
+
+class PreInscricaoForm(ModelForm):
+    class Meta:
+        model = PreInscricao
+        fields = ('__all__')
+        labels = {
+            'nome': 'Seu nome*',
+            'telefone': 'Número de telefone*',
+        }
+        widgets = {
+            'nome': forms.TextInput(attrs={'placeholder': 'Digite seu nome'}),
+            'telefone': forms.TextInput(attrs={'placeholder': '11981281099'}),
         }
